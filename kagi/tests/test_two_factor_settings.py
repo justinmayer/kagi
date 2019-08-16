5c5b35bb5b8215e3146ca64865027ccf9792e286
Rémy HUBSCHER <remy@chefclub.tv>
2019-08-16 17:49:50 +0200

    Fix flake8 linter errors.

--- kagi/tests/test_two_factor_settings.py
+++ kagi/tests/test_two_factor_settings.py
@@ -5,8 +5,8 @@ from django.urls import reverse
 def test_mfa_settings_page_displays_mfa_statuses(admin_client):
     response = admin_client.get(reverse("kagi:two-factor-settings"))
 
-    assert response.context_data["webauthn_enabled"] == False
-    assert response.context_data["totp_enabled"] == False
+    assert response.context_data["webauthn_enabled"] is False
+    assert response.context_data["totp_enabled"] is False
     assert response.context_data["backup_codes_count"] == 0
 
 
@@ -16,8 +16,8 @@ def test_mfa_settings_page_knows_when_webauthn_is_enabled(admin_client):
     user.webauthn_keys.create(key_name="SoloKey", sign_count=0)
     response = admin_client.get(reverse("kagi:two-factor-settings"))
 
-    assert response.context_data["webauthn_enabled"] == True
-    assert response.context_data["totp_enabled"] == False
+    assert response.context_data["webauthn_enabled"] is True
+    assert response.context_data["totp_enabled"] is False
     assert response.context_data["backup_codes_count"] == 0
 
 
@@ -27,8 +27,8 @@ def test_mfa_settings_page_knows_when_totp_is_enabled(admin_client):
     user.totp_devices.create()
     response = admin_client.get(reverse("kagi:two-factor-settings"))
 
-    assert response.context_data["webauthn_enabled"] == False
-    assert response.context_data["totp_enabled"] == True
+    assert response.context_data["webauthn_enabled"] is False
+    assert response.context_data["totp_enabled"] is True
     assert response.context_data["backup_codes_count"] == 0
 
 
@@ -38,6 +38,6 @@ def test_mfa_settings_page_knows_how_to_count_backup_codes(admin_client):
     user.backup_codes.create_backup_code()
     response = admin_client.get(reverse("kagi:two-factor-settings"))
 
-    assert response.context_data["webauthn_enabled"] == False
-    assert response.context_data["totp_enabled"] == False
+    assert response.context_data["webauthn_enabled"] is False
+    assert response.context_data["totp_enabled"] is False
     assert response.context_data["backup_codes_count"] == 1
