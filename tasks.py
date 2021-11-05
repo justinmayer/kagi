e5536dbd0d96a8e5706f9efd3a39f56257a7aa9c
Justin Mayer <entroP@gmail.com>
2021-11-05 11:09:17 -0500

    Improve Invoke task handling on Windows

--- tasks.py
+++ tasks.py
@@ -7,12 +7,14 @@ from invoke.util import cd
 
 DEMO_PORT = os.environ.get("DEMO_PORT", 8000)
 DOCS_PORT = os.environ.get("DOCS_PORT", 8000)
+BIN_DIR = "bin" if os.name != "nt" else "Scripts"
 PTY = True if os.name != "nt" else False
 
 ACTIVE_VENV = os.environ.get("VIRTUAL_ENV", None)
 VENV_HOME = Path(os.environ.get("WORKON_HOME", "~/.local/share/virtualenvs"))
 VENV_PATH = Path(ACTIVE_VENV) if ACTIVE_VENV else (VENV_HOME / "kagi")
 VENV = str(VENV_PATH.expanduser())
+VENV_BIN = Path(VENV) / Path(BIN_DIR)
 
 TOOLS = ["poetry", "pre-commit"]
 POETRY = which("poetry") if which("poetry") else (VENV / Path("bin") / "poetry")
@@ -24,7 +26,7 @@ PRECOMMIT = (
 @task
 def docs(c):
     """Build documentation"""
-    c.run(f"{VENV}/bin/sphinx-build docs docs/_build", pty=PTY)
+    c.run(f"{VENV_BIN}/sphinx-build docs docs/_build", pty=PTY)
 
 
 @task(docs)
@@ -43,14 +45,14 @@ def viewdocs(c):
 def serve(c):
     """Serve demo site at https://localhost:$DEMO_PORT/ (default port is 8000)"""
     with cd("testproj"):
-        c.run(f"{VENV}/bin/python manage.py runserver {DEMO_PORT} ", pty=PTY)
+        c.run(f"{VENV_BIN}/python manage.py runserver {DEMO_PORT} ", pty=PTY)
 
 
 @task
 def tests(c):
     """Run the test suite"""
     c.run(
-        f"{VENV}/bin/pytest -s --doctest-modules --cov-report term-missing "
+        f"{VENV_BIN}/pytest -s --doctest-modules --cov-report term-missing "
         "--cov-fail-under 100 --cov kagi",
         pty=PTY,
     )
@@ -60,14 +62,14 @@ def tests(c):
 def makemigrations(c):
     """Create database migrations if needed"""
     with cd("testproj"):
-        c.run(f"{VENV}/bin/python manage.py makemigrations", pty=PTY)
+        c.run(f"{VENV_BIN}/python manage.py makemigrations", pty=PTY)
 
 
 @task
 def migrate(c):
     """Migrate database to current schema"""
     with cd("testproj"):
-        c.run(f"{VENV}/bin/python manage.py migrate", pty=PTY)
+        c.run(f"{VENV_BIN}/python manage.py migrate", pty=PTY)
 
 
 @task
@@ -78,7 +80,7 @@ def black(c, check=False, diff=False):
         check_flag = "--check"
     if diff:
         diff_flag = "--diff"
-    c.run(f"{VENV}/bin/black {check_flag} {diff_flag} kagi testproj tasks.py", pty=PTY)
+    c.run(f"{VENV_BIN}/black {check_flag} {diff_flag} kagi testproj tasks.py", pty=PTY)
 
 
 @task
@@ -86,12 +88,12 @@ def isort(c, check=False):
     check_flag = ""
     if check:
         check_flag = "-c"
-    c.run(f"{VENV}/bin/isort {check_flag} .", pty=PTY)
+    c.run(f"{VENV_BIN}/isort {check_flag} .", pty=PTY)
 
 
 @task
 def flake8(c):
-    c.run(f"{VENV}/bin/flake8 kagi testproj tasks.py", pty=PTY)
+    c.run(f"{VENV_BIN}/flake8 kagi testproj tasks.py", pty=PTY)
 
 
 @task
@@ -106,7 +108,7 @@ def tools(c):
     """Install tools in the virtual environment if not already on PATH"""
     for tool in TOOLS:
         if not which(tool):
-            c.run(f"{VENV}/bin/pip install {tool}", pty=PTY)
+            c.run(f"{VENV_BIN}/python -m pip install {tool}", pty=PTY)
 
 
 @task
@@ -117,7 +119,7 @@ def precommit(c):
 
 @task
 def setup(c):
-    c.run(f"{VENV}/bin/pip install -U pip", pty=PTY)
+    c.run(f"{VENV_BIN}/python -m pip install -U pip", pty=PTY)
     tools(c)
     c.run(f"{POETRY} install", pty=PTY)
     precommit(c)
