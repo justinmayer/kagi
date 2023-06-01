61c1bce2a4e67065832665ee78b31749166eeac1
Florian Apolloner <florian@apolloner.eu>
2023-06-01 12:58:07 +0200

    Limit kagi to (Small)AutoField for now. Refs #55.

--- kagi/apps.py
+++ kagi/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class KagiConfig(AppConfig):
     name = "kagi"
+    default_auto_field = "django.db.models.AutoField"
 
     def monkeypatch_login_view(self):
         from .admin import monkeypatch_admin
