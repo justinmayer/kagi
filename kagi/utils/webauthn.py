4101e13bf199bfad1b60071bf4505236e5d3ba7f
Justin Mayer <entroP@gmail.com>
2023-06-02 13:59:30 +0200

    fixup! Port kagi to webauthn 1.6.0

--- kagi/utils/webauthn.py
+++ kagi/utils/webauthn.py
@@ -9,6 +9,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Origin: https://github.com/pypi/warehouse
 
 import base64
 import json
